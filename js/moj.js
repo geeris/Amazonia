82d8036ce569859ac4ed68e4bf6a4df434e57cea
Stefan <geeris77@gmail.com>
2021-09-06 21:00:21 +0200

    initial commit

---
+++ js/moj.js
@@ -0,0 +1,12 @@
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+  
